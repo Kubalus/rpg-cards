8793963fefd7e8bcdc8a3a828ec3795f8e9f2f54
Kubalus <jakub.bryniarski@gmail.com>
2019-12-01 01:00:40 +0100

    backend commit

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Choosing from './Choosing';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<Choosing />, document.getElementById('root'));
-
-serviceWorker.unregister();
